0c3eab4f550695efa6dec3dc79ac6a7d7754c6e6
Bloom <bloomtauta@gmail.com>
2023-02-25 23:10:51 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,25 @@
-class Dog
+# class Dog < ActiveRecord::Base
+# end
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/test.sqlite3"
+)
+
+# Create a Students table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the Student class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
 end
